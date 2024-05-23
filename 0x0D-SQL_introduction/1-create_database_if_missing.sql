68f4016bd1fa79b3167bd27804e1bd2d6d21c860
root <root@localhost.localdomain>
2024-05-23 03:42:36 +0300

    creates hbtn_0c_0

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- create a a database hbtn-0c-0 if it doesn't exist
 -- exit with exit code 0 if it already does
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0' ;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
