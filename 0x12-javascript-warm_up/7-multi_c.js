0ea9fe36a964555adc65a36e7aef7c8417b7b85e
IamGatheru <davidgatheru490@gmail.com>
2024-06-19 10:14:34 +0300

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fail = "Missing number of occurrences";
+let count = process.argv.length;
+let argument1 = process.argv
+
+if (count <= 2 || argument1[1] < 0) {
+  console.log(fail);
+};
+value = argument1[1];
+for (let x = 0; x < argument1[2]; x++) {
+    console.log(('C is fun'));
+}
