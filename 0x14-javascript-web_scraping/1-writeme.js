aaa9a2b3da2c2af89d9baa4f2082488973b65e41
IamGatheru <davidgatheru490@gmail.com>
2024-08-02 12:51:37 +0300

    Writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-// Retrieve commandline arguments
-const [,, filePath, content] = process.argv;
-
-// Check if both arguments are provided
-if (!filePath || !content) {
-  console.error('Usage: node script.js <filePath> <content>');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], err => {
   if (err) {
-    console.error('An error occurred:', err);
-    return;
+    console.log(err);
   }
-  console.log(`Content written to ${filePath}`);
 });
