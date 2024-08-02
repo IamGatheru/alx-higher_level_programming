295c614b47c81b080c1422992fbaa985e9672901
IamGatheru <davidgatheru490@gmail.com>
2024-08-02 13:20:42 +0300

    Star wars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,28 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-function getMovieTitleByEpisode (episodeNumber) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${episodeNumber}/`;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      console.log('Error fetching data:', error);
-      return;
-    }
-    if (response.statusCode === 200) {
-      const data = JSON.parse(body);
-      console.log('Title:', data.title);
-    } else {
-      console.error('Failed to retrieve data:', response.statusCode);
-    }
-  });
-}
-
-const episodeNumber = process.argv[2];
-
-if (episodeNumber) {
-  getMovieTitleByEpisode(episodeNumber);
-} else {
-  console.error('Please provide an episode number.');
-}
+const endPoint = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body && JSON.parse(body).title);
+  }
+});
