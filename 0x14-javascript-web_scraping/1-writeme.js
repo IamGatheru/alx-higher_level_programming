4bbedd43d70fd9a912aa5e909fc3d5436d79a850
IamGatheru <davidgatheru490@gmail.com>
2024-08-02 12:37:44 +0300

    Writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,19 +2,18 @@
 
 const fs = require('fs');
 // Retrieve commandline arguments
-const filePath = process.argv[2];
-const content = process.argv[3];
+const [,, filePath, content] = process.argv;
 
 // Check if both arguments are provided
 if (!filePath || !content) {
-  console.error('Please provide a file path and the content to write.');
+  console.error('Usage: node script.js <filePath> <content>');
   process.exit(1);
 }
-// Write the string to file
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+
+fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
-    console.log('Error writing file:', err);
+    console.error('An error occurred:', err);
     return;
   }
-  console.log('File written successfully');
+  console.log(`Content written to ${filePath}`);
 });
