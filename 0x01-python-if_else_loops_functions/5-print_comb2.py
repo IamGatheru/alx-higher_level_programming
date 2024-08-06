d66c037224549167bee760f7411469501fe036fc
IamGatheru <davidgatheru490@gmail.com>
2024-08-07 00:58:06 +0300

    00..99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i != 00 and i != 100:
-        print(',', end='')
-    print("{:02d}".format(i), end=' ')
+        print(',', end=' ')
+    print("{:02d}".format(i), end='')
