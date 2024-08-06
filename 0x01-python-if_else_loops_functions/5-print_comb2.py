3b472ef7ad361ac78631fb7e885ea46e4b358c97
IamGatheru <davidgatheru490@gmail.com>
2024-08-07 01:16:08 +0300

    00..99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 00 and i != 100:
-        print(', ', end='')
-    print("{:02d}".format(i), end='')
+print(", ".join("{:02d}".format(i) for i in range(0, 100)))
