a6bb4321339e189c6c71235994ad5c67573b6acd
IamGatheru <davidgatheru490@gmail.com>
2024-08-27 08:04:13 +0300

    Getter and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,7 +30,7 @@ class Square():
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
-        return self.__size
+        self.__size = value
 
     def area(self):
         """
